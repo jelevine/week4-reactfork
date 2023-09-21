bd6f77f58884373eabd4ebea5ce4123f6fad5b53
jelevine <jelevine21@gmail.com>
2023-09-21 12:54:39 -0400

    update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,26 @@ function App() {
 
 
     return (
-      <div className="App">
-        <h1>Hello World!</h1>
-        <h1>Welcome to my card</h1>
-        <img src="https://cdn.creatureandcoagency.com/uploads/2021/03/FrogLifecycle_Body_8.png" alt="Frog" />
-        <div className="paragraph">
-          <p>This is a picture of a frog that I found on the internet. It looks really cool, and I thought it would be nice to share.</p>
-        </div>
-        <div class="btn-wrapper">
- <a href="https://hax.psu.edu">
-   <button>Details</button> </a> </div>
-      </div>
+     <>
+     <div class = "buttons">
+  <button class = 'details' id = "nob">Clone</button>
+  <button class = 'details' id = 'delete'>Delete last</button>
+  <button class = 'details' id = 'background'>Change Background</button>
+  <button class = 'details' id = 'writing'>Change title</button>
+</div>
+<div class = "card">
+  <div class = "card-container">
+    <div class = "card-content">
+      <h2>Penn State Blue and White Game</h2>
+      <img align = "center" src = "https://dgiqkglfef83i.cloudfront.net/images/2023/4/15/Blue-White.png" alt = "B+W Game Image"/>
+      <p>With all hype on the upcoming football season, the Blue and White game gives the fans something to look forward to watching in the spring!</p>
+      <a class = 'details'>
+      <button id = 'details' >Details</button>
+      </a>
+    </div>
+  </div>
+</div>
+</>
     );
   }
 
